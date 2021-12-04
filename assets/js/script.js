7d388eeaa54972f60d06e889c3633135bc2aaf1b
Sodaman <mattrick2214@gmail.com>
2021-12-04 01:30:52 -0600

    Tidied up a few things, working on enterable tasks.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,38 @@
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-document.getElementById("currentDate").innerHTML = date;
-console.log(today)
+document.getElementById("currentDate").innerHTML = today;
+
+// var tasks = {};
+// var createTask = function(taskText, taskDate, taskList) {
+
+//     // Create elements that make a task.
+//     var taskLi = $("<li>").addClass("list-group-item");
+//     var taskSpan = $("<span>")
+//         .text(taskDate);
+//     var taskP = $("<p>")
+//         .text(taskText);
+//     // Append span and p element to parent li
+//     taskLi.append(taskSpan, taskP);
+
+//     // Append to UL list on the page.
+//     $("#list-" + taskList).append(taskLi);
+// };
+
+// var loadTasks = function() {
+//     tasks = JSON.parse(localStorage.getItem("tasks"));
+
+//     //If there is nothing to find, create a new object
+//     if (!tasks) {
+//         tasks = {
+            
+//         }
+//     }
+// };
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+
+//Load tasks for next time
+loadTasks();
