036670529cbe897ea4e988769a0a578b8d1c9755
Sodaman <mattrick2214@gmail.com>
2021-12-05 16:00:03 -0600

    attempted to get localStorage to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,14 @@
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours
 document.getElementById("currentDate").innerHTML = today;
+var textArea = document.querySelector(".description1")
+var buttons = document.querySelector(".saveBtn")
+var setHour = today.getHours
+const rows = document.getElementsByClassName(".row");
 
-$("#saveButton").on("click", function() {
-    const list_task = $(this).prev();
-    console.log(list_task.find("textarea").val());
-    localStorage.setItem("sixAM", list_task);
-});
-
-
-
-$(".list-group").on("click", "p", function() {
-    console.log("<p> was clicked");
+$(".row").on("click", "textarea", function() {
+    console.log("textarea was clicked");
+    console.log(rows)
     var text = $(this)
         .text()
         .trim()
@@ -21,9 +17,15 @@ $(".list-group").on("click", "p", function() {
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
 });
+var saveLocalStorage = function () {
+    var textArea1 = textArea.value
+    console.log(textArea.value)
+    localStorage.setItem("1", textArea1)
+}
 
-// if (list_task === time) {
-//     $(this).addClass("present");
-
-
-// }
+// $(".saveBtn").on("click", function() {
+//     const list_task = $(this).prev();
+//     console.log(list_task.find("textarea").val());
+//     localStorage.setItem("1", list_task);
+// });
+buttons.addEventListener("click", saveLocalStorage)
