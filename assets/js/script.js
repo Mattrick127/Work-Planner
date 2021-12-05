709165c46cb9613321df3f32cc8b29349e6b551f
Sodaman <mattrick2214@gmail.com>
2021-12-05 12:51:21 -0600

    opened my eyes to the css file and began to format around it, running into a few issues along the way.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,29 @@
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-document.getElementById("currentDate").innerHTML = today;
 var time = today.getHours
+document.getElementById("currentDate").innerHTML = today;
 
 $("#saveButton").on("click", function() {
     const list_task = $(this).prev();
-    console.log(list_task.find("textarea"));
-    console.log()
-})
+    console.log(list_task.find("textarea").val());
+    localStorage.setItem("sixAM", list_task);
+});
 
 
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-};
+
+$(".list-group").on("click", "p", function() {
+    console.log("<p> was clicked");
+    var text = $(this)
+        .text()
+        .trim()
+    var textInput = $("<textarea>")
+        .val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+// if (list_task === time) {
+//     $(this).addClass("present");
+
+
+// }
